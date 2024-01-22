c4a2e33549092407577fbf41e6374c3a1ba446be
fatimahnaj <145191276+fatimahnaj@users.noreply.github.com>
2024-01-22 23:24:48 +0800

    Add files via upload

---
+++ Text.py
@@ -0,0 +1,184 @@
+#choices
+choice_list = ([])
+
+choice_list.append('START')
+choice_list.append('sleep first')
+choice_list.append('play game')
+choice_list.append('skip')
+choice_list.append('take a bath')
+choice_list.append('play with the cat')
+choice_list.append('ignore the cat')
+choice_list.append('Accept Mr Winwin suggestion.')
+choice_list.append('Continue searching for a seat.')
+choice_list.append('Use matches')
+choice_list.append('Pinch yourself')
+choice_list.append('Make an escape')
+choice_list.append('Buy at pop up stall5')
+choice_list.append('Save money')
+choice_list.append('Go back to hostel ')
+choice_list.append('Walk around campus for a while')
+
+
+
+#dialogue
+dialogue_list = ([])
+#intro
+dialogue_list.append(
+    "Welcome to MMU-Life! What do you think MMU-Life stands for? My Memorable University Life? Or My Miserable Uni Life? "
+    )
+dialogue_list.append(
+    "You are a student in Multimedia University. Your university life becomes an exciting adventure filled with choices at every turn. "
+    )
+dialogue_list.append(
+    "What are you going to do? Navigate the twists and turns of a school day across three levels: before the class, in the classroom, and after the class."
+    )
+dialogue_list.append(
+    "Throughout your journey, every choice you make might earn you an extra heart or cost you some!"
+    )
+dialogue_list.append(
+    "Your mission is to navigate through the day, aiming to keep at least one heart by the end. "
+    )
+dialogue_list.append(
+    "Are you ready to start your journey? Let’s gooooo!!!"
+    )
+#bedroom scene
+dialogue_list.append(
+    "The night before your school day, you’re ready to go to your bed and sleep at 11pm because you have an early 8am class the next morning."
+    )
+dialogue_list.append(
+    "As you look around your room, you notice your roommate deeply engrossed in playing Genshin Impact. Suddenly, a mischievous idea sparks in your mind… "
+    )
+dialogue_list.append(
+    "“I have an early class tomorrow. I should stick to my sleep schedule.”"
+    )
+dialogue_list.append(
+    " “It’s just for a while, I’ll catch up on sleep later.” "
+    )
+dialogue_list.append(
+    "You end up playing Genshin until 3am..."
+    )
+dialogue_list.append(
+    "“SHOOT!! It’s almost morning-”"
+    )
+#morning scene
+dialogue_list.append(
+    "It’s the next day, and you’re thinking whether to take a bath or skip it because you are too lazy to bathe in the early morning. "
+    )
+dialogue_list.append(
+    " “I’ll smell fine right?”"
+    )
+dialogue_list.append(
+    "As you walk towards your class, you spot a cute cat ahead of you, seemingly eager for attention. It stops and looks at you with its big curious eyes..."
+    )
+dialogue_list.append(
+    "“Hi cutie, good morningg!!”"
+    )
+dialogue_list.append(
+    "..meow meoww (..and now you’re late for class)"
+    )
+dialogue_list.append(
+    "....."
+    )
+#entering class
+dialogue_list.append(
+    "You enter your classroom bustling with noise. Many students have already arrived, and every seat seems occupied."
+    )
+dialogue_list.append(
+    "You are trying hard searching for a spot to sit when the lecturer, Mr Winwin, notices your problem."
+    )
+dialogue_list.append(
+    "Why dont you grab a chair and sit at the front? Theres still a lot of space here. Dont be scared. I wont eat you ehehe."
+    )
+dialogue_list.append(
+    "“Sure, I’ll take a chair and sit at the front. It’s my pleasure :-) “"
+    )
+dialogue_list.append(
+    "Opt for a seat at the back, potentially escaping the lecturer’s watchful eye. (and wanna sit with your crush)"
+    )
+#class scene
+dialogue_list.append(
+    "During the lecture, you start dozing off and scrolling through memes on Twitter."
+    )
+dialogue_list.append(
+    "However, Mr Winwin is passionately teaching a complex topic, and this topic will come up in the final as well :-0. What will you do?"
+    )
+dialogue_list.append(
+    "Use matches to hold up your eyelids in order to keep your eyes open like what Mr Bean did to avoid dozing off while driving"
+    )
+dialogue_list.append(
+    "Pinching yourself, tapping fingers on the desk, shaking your leg and even leaning back on your chair to stay focused."
+    )
+dialogue_list.append(
+    "You miss your bed so so so much. So you ask Mr Winwin’s permission to go to the WC, pretending to be in an emergency…."
+    )
+dialogue_list.append(
+    "A suspicious look, an eyebrow raised are given by Mr Winwin."
+    )
+dialogue_list.append(
+    "He knows you're trying to escape his class under the guise of a toilet emergency, He then teasingly remarked,"
+    )
+dialogue_list.append(
+    "“Oh really? I suddenly feel a bit urgent too.”"
+    )
+#class ending
+dialogue_list.append(
+    "Finally, the class is coming to an end."
+    )
+dialogue_list.append(
+    "Before it dismisses, Mr Winwin, with his smirking face, says,"
+    )
+dialogue_list.append(
+    "“Alright, folks, before you dash off, I have a little surprise for you! Survive the unexpected pop quiz and leave the classroom alive!”"
+    )
+dialogue_list.append(
+    "The room filled with a sound of disappointment as students realized their happiness was interrupted."
+    )
+dialogue_list.append(
+    "Some students immediately delve into their notes, quickly scanning their notes and the materials."
+    )
+dialogue_list.append(
+    "quiz time !!!"
+    )
+#after class
+dialogue_list.append(
+    "You step out of the classroom, backpack slung over one shoulder, as the sun sets over the university campus."
+    )
+dialogue_list.append(
+    "You are so ready to head over to your room and just lay down lazily on your bed while scrolling through your phone."
+    )
+dialogue_list.append(
+    "But your mind changes when you see a bunch of crowds and stalls around the campus. "
+    )
+dialogue_list.append(
+    "“Should I buy something first ?”"
+    )
+dialogue_list.append(
+    "You’ve seen a takoyaki stall and instantly head there to make your purchase."
+    )
+dialogue_list.append(
+    "When you open your wallet, you realise that this is the last cash that you own - “I had overspent!”"
+    )
+dialogue_list.append(
+    "“Maybe not, I almost reached my budget.”"
+    )
+dialogue_list.append(
+    "The day is all yours now. What would you do after this?"
+    )
+dialogue_list.append(
+    "You decide to walk around campus for a while when suddenly someone taps you at the back."
+    )
+dialogue_list.append(
+    "“Whatsup! Going to the meeting already?”"
+    )
+dialogue_list.append(
+    "Meeting? THAT'S RIGHT! I almost shouted when I realised I had a group meeting today."
+    )
+dialogue_list.append(
+    "I head to the group discussion with my friend."
+    )
+dialogue_list.append(
+    "You feel super tired today so you head straight to the hostel."
+    )
+dialogue_list.append(
+    "... Unfortunately, you have forgotten your business group discussion."
+    )
